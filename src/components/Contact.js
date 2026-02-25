25364b8fa191de613fd9177916af488b0f1cc6c3
4me7h <4573r10n@gmail.com>
2026-02-24 21:54:47 -0600

    style: contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,35 +20,29 @@ const Contact = () => {
                         <div className="headercontent__container">
                             <div className="headercontent__header">
                                 <Fade cascade triggerOnce direction="up"> 
-                                <div className="about__header">
-                                    <h2 className="text--subtitle text-center text--dark-primary">{t('contact-title')}</h2>
-                                    <h4 className="text--slogan text--dark-secondary">
-                                        {t('contact-subtitle')}
-                                    </h4>
-                                    <h4 className="text--lead text--dark-secondary-hover">
-                                        {t('contact-subtitle-02')}
-                                    </h4>
-                                </div>
-                               
-                                {/* <ul className="contact__buttons">
-                                    <li className='contact__buttons__item'>
-                                        <a className="btn btn--secondary" target="__blank" href="https://wa.me/+5219613037177?text=Lobo%20Tours:%20Estoy%20interesado%20en%20un%20viaje%20de%20clase%20económica">Reserva Aquí</a>
-                                    </li>
-                                    <li className="contact__buttons__item">
-                                        <a className="btn btn--secondary" target="__blank" href="https://wa.me/+5219613037177?text=Lobo%20Tours:%20Estoy%20interesado%20en%20un%20viaje%20de%20clase%20económica">Reserva Aquí</a>
-                                    </li>
-                                </ul> */}
+                                    <div className="about__header">
+                                        <h2 className="text--subtitle text-center text--dark-primary">{t('contact-title')}</h2>
+                                        <h4 className="text--slogan text--dark-secondary">
+                                            {t('contact-subtitle')}
+                                        </h4>
+                                        <h4 className="text--lead text--dark-secondary-hover">
+                                            {t('contact-subtitle-02')}
+                                        </h4>
+                                    </div>
                                 </Fade>
                             </div>
                         </div>
                     </Col>
                 </Row>
                 <Row className='d-flex'>
-                    <Fade cascade triggerOnce direction="left"> 
+                    
                     <Col md={6} className='d-flex mb-5'>
-                        <MapComponent />
+                        <Fade triggerOnce direction="right"> 
+                            <MapComponent />
+                        </Fade>
                     </Col>
                     <Col md={6} className='d-flex mb-5'>
+                        <Fade cascade triggerOnce direction="left"> 
                         <ul className="contact__information">
                             <li className='contact__information__item'>
                                 <span className='contact__information__title'>
@@ -76,13 +70,16 @@ const Contact = () => {
                                 </span>
                             </li>
                         </ul>
+                        </Fade>
                     </Col>
                     <Col>
                         <div className="contact__content text-center">
-                            <a className="btn btn--secondary btn--secondary--dark" target="__blank" href="https://wa.me/+17273405713?text=Hi,%20I'm%20interested%20in%20the%20remodelling%20service">{t('contact-call')}</a>
+                            <Fade triggerOnce direction="bottom"> 
+                                <a className="btn btn--secondary btn--secondary--dark" target="__blank" href="https://wa.me/+17273405713?text=Hi,%20I'm%20interested%20in%20the%20remodelling%20service">{t('contact-call')}</a>
+                            </Fade>
                         </div>
                     </Col>
-                    </Fade>
+                   
                 </Row>
             </Container>
         </section>
